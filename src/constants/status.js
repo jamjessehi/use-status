87bcd7b55abccbe3c55986cb835bcacdf8898ab4
jamjessehi <jec2mt69e@163.com>
2020-03-30 12:02:38 +0800

    useReducer instead of useState

--- src/constants/status.js
+++ src/constants/status.js
@@ -2,3 +2,11 @@ export const IDLE = "IDLE";
 export const PENDING = "PENDING";
 export const RESOLVE = "RESOLVE";
 export const REJECT = "REJECT";
+
+export default state => ({
+  isLoading: state === IDLE || state === PENDING,
+  isIdle: state === IDLE,
+  isPending: state === PENDING,
+  isResolved: state === RESOLVE,
+  isRejected: state === REJECT
+});
--- src/useStatus.js
+++ src/useStatus.js
@@ -1,44 +1,71 @@
-import { useState, useMemo, useCallback } from "react";
-import { IDLE, PENDING, RESOLVE, REJECT } from "constants/status";
+import { useReducer, useMemo, useCallback } from "react";
+
+import deriveFromStatus, {
+  IDLE,
+  PENDING,
+  RESOLVE,
+  REJECT
+} from "constants/status";
+
+const REQUEST = "REQUEST";
+const RECEIVE = "RECEIVE";
+const ERROR = "ERROR";
+
+const initialState = {
+  data: undefined,
+  status: IDLE,
+  error: false
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case REQUEST:
+      return {
+        ...state,
+        error: false,
+        status: PENDING
+      };
+
+    case RECEIVE:
+      return {
+        ...state,
+        status: RESOLVE,
+        data: action.payload,
+        error: false
+      };
+
+    case ERROR:
+      return {
+        ...state,
+        status: REJECT,
+        error: action.payload
+      };
+
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
+
+const requestAction = () => ({ type: REQUEST });
+
+const receiveAction = payload => ({ type: RECEIVE, payload });
+
+const failAction = payload => ({ type: ERROR, payload });
 
 export default () => {
-  const [state, setState] = useState(IDLE);
-  const [data, setData] = useState();
-  const [error, setError] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const status = useMemo(
-    () => ({
-      isLoading: state === IDLE || state === PENDING,
-      isIdle: state === IDLE,
-      isPending: state === PENDING,
-      isResolved: state === RESOLVE,
-      isRejected: state === REJECT
-    }),
-    [state]
-  );
+  const status = useMemo(() => deriveFromStatus(state.status), [state.status]);
 
-  const resolve = useCallback(data => {
-    setState(RESOLVE);
-    setData(data);
-    setError(false);
-  }, []);
-
-  const reject = useCallback(error => {
-    setState(REJECT);
-    setError(error);
-  }, []);
-
-  const pending = useCallback(() => {
-    setError(false);
-    setState(PENDING);
-  }, []);
+  const request = useCallback(() => dispatch(requestAction()), []);
+  const receive = useCallback(payload => dispatch(receiveAction(payload)), []);
+  const fail = useCallback(payload => dispatch(failAction(payload)), []);
 
   return {
+    state,
     status,
-    data,
-    error,
-    resolve,
-    reject,
-    pending
+    request,
+    receive,
+    fail
   };
 };
